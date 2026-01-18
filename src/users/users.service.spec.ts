d940e5ff2e3e2d8e5ebe21b01224c353eb466774
maria45889 <mariajosetacoc2005@gmail.com>
2026-01-18 16:42:52 -0500

    Tests users

---
+++ src/users/users.service.spec.ts
@@ -0,0 +1,31 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from './users.service';
+
+describe('UsersService', () => {
+  let service: UsersService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UsersService,
+        {
+          provide: 'UserRepository',
+          useValue: {
+            create: jest.fn(),
+            save: jest.fn(),
+            find: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<UsersService>(UsersService);
+  });
+
+  it('debe estar definido', () => {
+    expect(service).toBeDefined();
+  });
+});
